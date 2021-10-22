aa5d086f02bd6a01e33ac1a8b674c85445d42e72
Guga Zimmermann <gugazimmermann@gmail.com>
2021-10-22 16:16:40 -0300

    fix models to es6

--- sequelize/models/invites.js
+++ sequelize/models/invites.js
@@ -1,6 +1,5 @@
-"use strict";
-const { Model } = require("sequelize");
-module.exports = (sequelize, DataTypes) => {
+import { Model } from "sequelize";
+export default (sequelize, DataTypes) => {
   class Invites extends Model {
     /**
      * Helper method for defining associations.
