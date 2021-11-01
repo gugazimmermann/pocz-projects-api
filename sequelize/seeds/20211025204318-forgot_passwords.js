fa944655d7ebaf8268edc208df7910edfd5acf09
Guga Zimmermann <gugazimmermann@gmail.com>
2021-11-01 13:31:05 -0300

    fix test conflict

--- sequelize/seeds/20211025204318-forgot_passwords.js
+++ sequelize/seeds/20211025204318-forgot_passwords.js
@@ -18,6 +18,15 @@ module.exports = {
           updatedAt: new Date(),
         },
         {
+          id: "a277dfdd-fc73-464e-8509-242681311f19",
+          email: "test@test.com",
+          code: "9876",
+          codeurl: "31c5502a-56de-443f-9e67-9aa1be26b294",
+          expiryDate: expiryDate.toJSDate(),
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+        {
           id: "51bcc414-33e3-4ee4-8812-8ff08ce76eaa",
           email: "teste-not-found@test.com",
           code: "5678",
--- services/auth-api/test/forgot-password-code.test.js
+++ services/auth-api/test/forgot-password-code.test.js
@@ -34,11 +34,11 @@ describe("Auth API - Forgot Password Code", () => {
 
   test("Should success", async () => {
     const res = await forgotPasswordCode.handler({
-      body: JSON.stringify({ codeurl: "da549802-a6be-4b87-ba8c-e46632135acf" }),
+      body: JSON.stringify({ codeurl: "31c5502a-56de-443f-9e67-9aa1be26b294" }),
     });
     const body = JSON.parse(res.body);
     expect(res.statusCode).toEqual(200);
-    expect(body.code).toBe(1234);
+    expect(body.code).toBe(9876);
   });
 
   test("Should return error wihtout database", async () => {
