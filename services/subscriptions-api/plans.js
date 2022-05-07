5c80f23f5691bf7c08397ed21b4f2278d383215e
Guga Zimmermann <gugazimmermann@gmail.com>
2022-05-07 00:08:31 -0300

    fix user not needed

--- services/subscriptions-api/plans.js
+++ services/subscriptions-api/plans.js
@@ -4,7 +4,6 @@ import CreateResponse from "../../libs/response";
 export const handler = async (event, context) => {
   try {
     const { Plans } = await database();
-    if (user instanceof Error) return CreateResponse( user.statusCode, { message: user.message });
     const data = await Plans.findAll();
     return CreateResponse(200, { data });
   } catch (err) {
