42dab2a9d2f3469fd3abf2c97ae55616ef58579a
Guga Zimmermann <gugazimmermann@gmail.com>
2022-06-20 21:58:10 -0300

    fix employeesList and managersList

--- services/places-api/employees.test.js
+++ services/places-api/employees.test.js
@@ -3,31 +3,31 @@ import { Tokens, createEvent } from "../../libs/test-utils";
 import { LambdaTypes, handler } from "./index";
 
 const placeID = "099330a1-ddb2-402b-8560-a95848b69033";
-const employeesObj = [{id: "a5109879-c5c0-48d2-9f9b-f17450b89c7d"}, {id: "4e573fa5-00b3-45cf-bdf5-c88ddf8cbabe"}];
+const employeesList = [{id: "a5109879-c5c0-48d2-9f9b-f17450b89c7d"}, {id: "4e573fa5-00b3-45cf-bdf5-c88ddf8cbabe"}];
 
 describe("Places API - Employees", () => {
   afterAll(() => { close() });
 
   test("Should fail without ID", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Employees, employeesObj, Tokens.Valid, {}));
+    const res = await handler(await createEvent(LambdaTypes.Employees, { employeesList }, Tokens.Valid, {}));
     expect(res.statusCode).toEqual(400);
     expect(JSON.parse(res.body).message).toBe("Dados inválidos!");
   });
 
   test("Should fail without body", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Employees, null, Tokens.Valid, { id: placeID }));
+    const res = await handler(await createEvent(LambdaTypes.Employees, {}, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(400);
     expect(JSON.parse(res.body).message).toBe("Dados inválidos!");
   });
 
   test("Should fail with invalid ID", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Employees, employeesObj, Tokens.Valid, { id: 'ae4e262e-545c-4165-ad6c-068a28008c59' }));
+    const res = await handler(await createEvent(LambdaTypes.Employees, { employeesList }, Tokens.Valid, { id: 'ae4e262e-545c-4165-ad6c-068a28008c59' }));
     expect(res.statusCode).toEqual(404);
     expect(JSON.parse(res.body).message).toBe("Registro não encontrado!");
   });
 
   test("Should success", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Employees, employeesObj, Tokens.Valid, { id: placeID }));
+    const res = await handler(await createEvent(LambdaTypes.Employees, { employeesList }, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(200);
     expect(JSON.parse(res.body).body.id).toBe(placeID);
   });
@@ -35,7 +35,7 @@ describe("Places API - Employees", () => {
   test("Should return database error", async () => {
     const { Places } = await database();
     const mock = jest.spyOn(Places, 'findOne').mockRejectedValueOnce(new Error("DB ERROR!"));
-    const res = await handler(await createEvent(LambdaTypes.Employees, employeesObj, Tokens.Valid, { id: placeID }));
+    const res = await handler(await createEvent(LambdaTypes.Employees, { employeesList }, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(500);
     expect(JSON.parse(res.body).message).toBe("DB ERROR!");
     mock.mockRestore();
--- services/places-api/managers.test.js
+++ services/places-api/managers.test.js
@@ -3,31 +3,31 @@ import { Tokens, createEvent } from "../../libs/test-utils";
 import { LambdaTypes, handler } from "./index";
 
 const placeID = "099330a1-ddb2-402b-8560-a95848b69033";
-const managersObj = [{id: "fd6bc51e-195e-4433-b404-8a9fdfa0f632"}];
+const managersList = [{id: "fd6bc51e-195e-4433-b404-8a9fdfa0f632"}];
 
 describe("Places API - Managers", () => {
   afterAll(() => { close() });
 
   test("Should fail without ID", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Managers, managersObj, Tokens.Valid, {}));
+    const res = await handler(await createEvent(LambdaTypes.Managers, { managersList }, Tokens.Valid, {}));
     expect(res.statusCode).toEqual(400);
     expect(JSON.parse(res.body).message).toBe("Dados inválidos!");
   });
 
-  test("Should fail without managersObj", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Managers, null, Tokens.Valid, { id: placeID }));
+  test("Should fail without body", async () => {
+    const res = await handler(await createEvent(LambdaTypes.Managers, {}, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(400);
     expect(JSON.parse(res.body).message).toBe("Dados inválidos!");
   });
 
   test("Should fail with invalid ID", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Managers, managersObj, Tokens.Valid, { id: 'ae4e262e-545c-4165-ad6c-068a28008c59' }));
+    const res = await handler(await createEvent(LambdaTypes.Managers, { managersList }, Tokens.Valid, { id: 'ae4e262e-545c-4165-ad6c-068a28008c59' }));
     expect(res.statusCode).toEqual(404);
     expect(JSON.parse(res.body).message).toBe("Registro não encontrado!");
   });
 
   test("Should success", async () => {
-    const res = await handler(await createEvent(LambdaTypes.Managers, managersObj, Tokens.Valid, { id: placeID }));
+    const res = await handler(await createEvent(LambdaTypes.Managers, { managersList }, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(200);
     expect(JSON.parse(res.body).body.id).toBe(placeID);
   });
@@ -35,7 +35,7 @@ describe("Places API - Managers", () => {
   test("Should return database error", async () => {
     const { Places } = await database();
     const mock = jest.spyOn(Places, 'findOne').mockRejectedValueOnce(new Error("DB ERROR!"));
-    const res = await handler(await createEvent(LambdaTypes.Managers, managersObj, Tokens.Valid, { id: placeID }));
+    const res = await handler(await createEvent(LambdaTypes.Managers, { managersList }, Tokens.Valid, { id: placeID }));
     expect(res.statusCode).toEqual(500);
     expect(JSON.parse(res.body).message).toBe("DB ERROR!");
     mock.mockRestore();
